15d1902b245d353db3f0b4fc41b9dec8cd4fa8b6
小帅丶 <578274480@qq.com>
2018-04-24 14:23:04 +0800

    :sparkles:新增微信aes算法工具类，微信常量类

---
+++ src/main/java/cn/xsshome/mvcdo/util/WeChatConstant.java
@@ -0,0 +1,10 @@
+package cn.xsshome.mvcdo.util;
+/**
+ * 微信相关常量值
+ * @author 小帅丶
+ *
+ */
+public class WeChatConstant {
+	static final String WCSP_APPID="";
+	static final String WCSP_APPSECRET="";
+}
