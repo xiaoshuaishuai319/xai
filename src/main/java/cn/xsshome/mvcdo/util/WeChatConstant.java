a5150bdb6fced9cc2b200742b8d50cc6d6017a30
小帅丶 <578274480@qq.com>
2018-04-24 14:32:39 +0800

    :sparkles:更新常量类属性

--- src/main/java/cn/xsshome/mvcdo/util/WeChatConstant.java
+++ src/main/java/cn/xsshome/mvcdo/util/WeChatConstant.java
@@ -2,9 +2,22 @@ package cn.xsshome.mvcdo.util;
 /**
  * 微信相关常量值
  * @author 小帅丶
- *
  */
 public class WeChatConstant {
-	static final String WCSP_APPID="";
-	static final String WCSP_APPSECRET="";
+	/**
+	 * 微信小程序的APPID
+	 */
+	public static final String WCSP_APPID="APPID";
+	/**
+	 * 微信小程序的APPSECRET
+	 */
+	public static final String WCSP_APPSECRET="APPSECRET";
+	/**
+	 * 获取session_key秘钥接口地址
+	 */
+	public static final String JSCODE2SESSION_URL="https://api.weixin.qq.com/sns/jscode2session";
+	/**
+	 * 授权类型
+	 */
+	public static final String GRANT_TYPE="authorization_code";
 }
